85dccd9fdc605082b0ccf498a0bb9595f4cd7278
Eduardo Sthory <sthory@hotmail.com>
2019-08-25 21:40:44 -0400

    Update LogReg.scala

--- LogReg.scala
+++ LogReg.scala
@@ -1,8 +1,8 @@
 //////////////////////////////////////////////
-// LOGISTIC REGRESSION PROJECT //////////////
+// LOGISTIC REGRESSION            ///////////
 ////////////////////////////////////////////
 
-//  In this project we will be working with a fake advertising data set, indicating whether or not a particular internet user clicked on an Advertisement. We will try to create a model that will predict whether or not they will click on an ad based off the features of that user.
+//  We will be working with a fake advertising data set, indicating whether or not a particular internet user clicked on an Advertisement. We will try to create a model that will predict whether or not they will click on an ad based off the features of that user.
 //  This data set contains the following features:
 //    'Daily Time Spent on Site': consumer time on site in minutes
 //    'Age': cutomer age in years
@@ -14,12 +14,6 @@
 //    'Country': Country of consumer
 //    'Timestamp': Time at which consumer clicked on Ad or closed window
 //    'Clicked on Ad': 0 or 1 indicated clicking on Ad
-
-///////////////////////////////////////////
-// COMPLETE THE COMMENTED TASKS BELOW ////
-/////////////////////////////////////////
-
-
 
 ////////////////////////
 /// GET THE DATA //////
